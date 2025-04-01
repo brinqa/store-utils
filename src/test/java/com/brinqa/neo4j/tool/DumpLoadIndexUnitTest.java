6cd55689c08570eca2469a919850bb7704e57344
Will Droste <will.droste@brinqa.com>
2025-04-01 01:11:17 -0500

    spotless

--- src/test/java/com/brinqa/neo4j/tool/DumpLoadIndexUnitTest.java
+++ src/test/java/com/brinqa/neo4j/tool/DumpLoadIndexUnitTest.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.util.List;
-
 import org.junit.Ignore;
 import org.junit.Test;
 
